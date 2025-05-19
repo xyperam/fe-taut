9142e405d6015e4d4d39cfc84f7c10e1b8277d51
Ilham Imani Nur Alam <ilhamimaninuralam@gmail.com>
2025-05-19 22:51:57 +0700

    navbar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,10 +1,30 @@
 'use client';
 import NavigationItem from "./NavigationItem";
 import { navItems } from "./navItem";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 export default function Sidebar(){
-    return(
+  
+  return(
         <aside className="hidden md:flex flex-col w-64 bg-amber-700">
-            <h2 className="text-lg font-bold mb-4">TAUT</h2>
+            <h2 className="text-xl font-bold mb-4 ml-4 mt-4">TAUT</h2>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+            <div className="flex flex-row w-full gap-2 ml-4">
+            <Avatar>
+             <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <h1>IlhamImani</h1>
+
+            </div>
+                </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-64 bg-amber-50">
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+            </DropdownMenu>
             <nav className="flex flex-col gap-2">
                  {navItems.map((item) => (
           <NavigationItem key={item.path} {...item} orientation="vertical" />
